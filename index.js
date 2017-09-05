279af097392350ea0cbf3c5ace398d7ab77b20a0
Deni Cho <dencho.12@gmail.com>
2017-09-05 08:41:18 +0700

    Added basic reply test

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+require('dotenv').config();
 
+const Telegraf = require('telegraf');
+const app = new Telegraf(process.env.BOT_TOKEN);
+
+app.hears('hi', ctx => {
+  return ctx.reply('Kontol....');
+});
+
+app.startPolling();
